116dd49c6131f840b188b097f54c2dc35aa7adbf
Adam Ruehl <adam.ruehl@lairdconnect.com>
2020-06-12 09:01:51 -0400

    mg100: fix further whitespace issues with ble_motion_service.c
    this fixes further tab issues and newline issues.
    
    Bug 16813

--- mg100/src/ble_motion_service.c
+++ mg100/src/ble_motion_service.c
@@ -18,7 +18,7 @@ LOG_MODULE_REGISTER(mg100_motion_svc);
 #define MOTION_SVC_LOG_INF(...) LOG_INF(__VA_ARGS__)
 #define MOTION_SVC_LOG_DBG(...) LOG_DBG(__VA_ARGS__)
 
-#define MOTION_ALARM_ACTIVE     1
+#define MOTION_ALARM_ACTIVE		1
 #define MOTION_ALARM_INACTIVE   0
 #define INACTIVITY_TIMER_PERIOD K_MSEC(30000)
 #define MOTION_DEFAULT_THS  8
@@ -40,13 +40,13 @@ LOG_MODULE_REGISTER(mg100_motion_svc);
 /******************************************************************************/
 #define MOTION_SVC_BASE_UUID_128(_x_)                                           \
 	BT_UUID_INIT_128(0x66, 0x9a, 0x0c, 0x20, 0x00, 0x08, 0x6e, 0x8b, 0xea, 0x11, \
-            0x1a, 0xac, LSB_16(_x_), MSB_16(_x_), 0xce, 0xad)
+			0x1a, 0xac, LSB_16(_x_), MSB_16(_x_), 0xce, 0xad)
 
 static struct bt_uuid_128 MOTION_SVC_UUID = MOTION_SVC_BASE_UUID_128(0x0a30);
 static struct bt_uuid_128 MOTION_ALARM_UUID = MOTION_SVC_BASE_UUID_128(0x0a31);
 
 struct ble_motion_service {
-    u16_t motion_alarm_index;
+	u16_t motion_alarm_index;
 	u8_t motion_alarm;
 };
 
@@ -66,7 +66,7 @@ static struct k_timer motion_timer;
 /* Local Function Prototypes                                                  */
 /******************************************************************************/
 static void motion_alarm_ccc_handler(const struct bt_gatt_attr *attr,
-                    u16_t value);
+					u16_t value);
 static void motion_sensor_trig_handler(struct device *dev,
 					struct sensor_trigger *trigger);
 static void motion_timer_callback(struct k_timer *timer_id);
@@ -103,9 +103,9 @@ static void motion_svc_notify(bool notify, u16_t index, u16_t length)
 	if (connection_handle != NULL) {
 		if (notify) {
 			bt_gatt_notify(connection_handle,
-				       &motion_svc.attrs[index],
-				       motion_svc.attrs[index].user_data,
-				       length);
+						&motion_svc.attrs[index],
+						motion_svc.attrs[index].user_data,
+						length);
 		}
 	}
 }
@@ -119,12 +119,12 @@ void motion_svc_set_alarm_state(u8_t alarmState)
 
 void motion_svc_init()
 {
-    struct sensor_trigger trigger;
+	struct sensor_trigger trigger;
 	struct device *sensor = device_get_binding(DT_ST_LIS2DH_0_LABEL);
 	struct sensor_value sVal;
 	int status = 0;
 
-    bms.motion_alarm = MOTION_ALARM_INACTIVE;
+	bms.motion_alarm = MOTION_ALARM_INACTIVE;
 	bt_gatt_service_register(&motion_svc);
 	size_t gatt_size = (sizeof(motion_attrs) / sizeof(motion_attrs[0]));
 	bms.motion_alarm_index = lbt_find_gatt_index(&MOTION_ALARM_UUID.uuid, motion_attrs,
@@ -165,16 +165,16 @@ static void motion_alarm_ccc_handler(const struct bt_gatt_attr *attr, u16_t valu
 
 static void motion_timer_callback(struct k_timer *timer_id)
 {
-    k_timer_stop(&motion_timer);
-    motion_svc_set_alarm_state(MOTION_ALARM_INACTIVE);
+	k_timer_stop(&motion_timer);
+	motion_svc_set_alarm_state(MOTION_ALARM_INACTIVE);
 }
 
 void motion_sensor_trig_handler(struct device *dev,
 					 struct sensor_trigger *trigger)
 {
-    k_timer_stop(&motion_timer);
-    motion_svc_set_alarm_state(MOTION_ALARM_ACTIVE);
-    k_timer_start(&motion_timer, INACTIVITY_TIMER_PERIOD,
-			      INACTIVITY_TIMER_PERIOD);
+	k_timer_stop(&motion_timer);
+	motion_svc_set_alarm_state(MOTION_ALARM_ACTIVE);
+	k_timer_start(&motion_timer, INACTIVITY_TIMER_PERIOD,
+				INACTIVITY_TIMER_PERIOD);
 	MOTION_SVC_LOG_DBG("Movement of the gateway detected.");
 }

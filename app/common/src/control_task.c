28b779a5b34911cb8e05bd008b8f67e7f42329f4
Andrew Hedin <andrew.hedin@lairdconnect.com>
2021-05-27 11:40:36 -0500

    Update sensor table when decommissioning
    Bug 19060
    Doesn't apply to CT because it doesn't have a sensor task

--- app/common/src/control_task.c
+++ app/common/src/control_task.c
@@ -394,8 +394,10 @@ static void commission_handler(void)
 	attr_set_signed32(ATTR_ID_certStatus, CERT_STATUS_BUSY);
 	attr_set_uint32(ATTR_ID_commissioningBusy, true);
 
+#ifdef CONFIG_SENSOR_TASK
 	FRAMEWORK_MSG_CREATE_AND_SEND(FWK_ID_CONTROL_TASK, FWK_ID_SENSOR_TASK,
 				      FMC_AWS_DECOMMISSION);
+#endif
 
 	/* If the value is written, then always decommission so that the connection
 	 * is closed and the certs are unloaded.  The files aren't deleted.

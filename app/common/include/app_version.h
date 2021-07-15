872e9e7ac1361000c8be284b6d006c837c2ff304
thedjnK <spam@helper3000.net>
2021-07-15 15:05:05 +0100

    Bug #19398 Upissue to version 5.1.1

--- app/common/include/app_version.h
+++ app/common/include/app_version.h
@@ -18,7 +18,7 @@ extern "C" {
 /******************************************************************************/
 #define APP_VERSION_MAJOR 5
 #define APP_VERSION_MINOR 1
-#define APP_VERSION_PATCH 0
+#define APP_VERSION_PATCH 1
 #define APP_VERSION_STRING                                                     \
 	STRINGIFY(APP_VERSION_MAJOR)                                           \
 	"." STRINGIFY(APP_VERSION_MINOR) "." STRINGIFY(APP_VERSION_PATCH)

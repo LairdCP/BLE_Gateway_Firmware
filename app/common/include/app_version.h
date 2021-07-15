acba30e51026bd65603727ca2d977da271bc5cea
thedjnK <spam@helper3000.net>
2021-07-15 10:31:13 +0100

    Bug #19393 Update version number to 5.1.0

--- app/common/include/app_version.h
+++ app/common/include/app_version.h
@@ -2,7 +2,7 @@
  * @file app_version.h
  * @brief
  *
- * Copyright (c) 2020 Laird Connectivity
+ * Copyright (c) 2020-2021 Laird Connectivity
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -17,7 +17,7 @@ extern "C" {
 /* Application Firmware Version                                               */
 /******************************************************************************/
 #define APP_VERSION_MAJOR 5
-#define APP_VERSION_MINOR 0
+#define APP_VERSION_MINOR 1
 #define APP_VERSION_PATCH 0
 #define APP_VERSION_STRING                                                     \
 	STRINGIFY(APP_VERSION_MAJOR)                                           \

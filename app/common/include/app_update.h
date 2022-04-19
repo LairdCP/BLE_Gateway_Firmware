28057a7171dce22861b87eea2b3b6c0e7864c55d
Andrew Hedin <andrew.hedin@lairdconnect.com>
2022-04-19 15:47:32 -0500

    app_update: Add module
    Allows the app to be updated (slot 2) from the file system.

---
+++ app/common/include/app_update.h
@@ -0,0 +1,32 @@
+/**
+ * @file app_update.h
+ * @brief
+ *
+ * Copyright (c) 2022 Laird Connectivity
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef __APP_UPDATE_H__
+#define __APP_UPDATE_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/******************************************************************************/
+/* Global Function Prototypes                                                 */
+/******************************************************************************/
+/**
+ * @brief Initiate application update from a file on the file system.
+ * Copy file into slot 2.  Set Memfault tracking flags. Issue reset.
+ *
+ * @param abs_path Absolute path/name of file
+ * @return int 0 on success, otherwise negative error code
+ */
+int app_update_initiate(const char *abs_path);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __APP_UPDATE_H__ */
